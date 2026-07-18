2e3032e08ba3cfc1872cd8a3ec84e04659bfe3c1
dipujafar <dipujafarmr@gmail.com>
2026-07-18 09:29:10 +0600

    change earning part

--- src/app/admin/dashboard/_components/StatContainer.tsx
+++ src/app/admin/dashboard/_components/StatContainer.tsx
@@ -23,7 +23,7 @@ const StatContainer = ({ data }: { data: any }) => {
   
 
   return (
-    <div className={`grid grid-cols-1 lg:grid-cols-3 gap-4 `}>
+    <div className={`grid grid-cols-1 lg:grid-cols-2 gap-4 `}>
       <StatCard
         title='Active User'
         value={data?.activeUsers}
